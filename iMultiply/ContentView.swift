c83b2280b06d8d174ba68d15e1764b56e4ebca4b
Lucas Chae <hi@lucaschae.com>
2022-05-05 16:48:23 +0900

    Initial Commit

---
+++ iMultiply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMultiply
+//
+//  Created by Lucas Chae on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMultiply/iMultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMultiplyApp.swift
+//  iMultiply
+//
+//  Created by Lucas Chae on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct iMultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
